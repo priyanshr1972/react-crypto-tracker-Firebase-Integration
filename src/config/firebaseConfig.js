3e07ed33919f108e9d97d42870849552d628da25
priyanshr1972 <priyanshr1972@gmail.com>
2023-05-03 12:33:29 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAj6KxcNL_E-nFzpapmXcW5-be8gyOxNWE",
+    authDomain: "react-crypto-tracker-master.firebaseapp.com",
+    projectId: "react-crypto-tracker-master",
+    storageBucket: "react-crypto-tracker-master.appspot.com",
+    messagingSenderId: "543562105261",
+    appId: "1:543562105261:web:5e398e6bfa98334335e124",
+    measurementId: "G-84VR9C7ZFV"
+};
+
+export default firebaseConfig;
